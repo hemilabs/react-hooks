dff56fe159c56c835706e83678060f66c5a962eb
Gonzalo D'elia <gonzalo@hemi.xyz>
2026-05-14 17:35:19 -0300

    Provide SSR snapshot to useSyncExternalStore

--- src/useVisualViewportSize/index.ts
+++ src/useVisualViewportSize/index.ts
@@ -3,7 +3,7 @@ import { useSyncExternalStore } from "react";
 function subscribe(callback: (this: VisualViewport, ev: Event) => unknown) {
   window.visualViewport?.addEventListener("resize", callback);
   window.visualViewport?.addEventListener("scroll", callback);
-  return function () {
+  return function cleanup() {
     window.visualViewport?.removeEventListener("resize", callback);
     window.visualViewport?.removeEventListener("scroll", callback);
   };
@@ -20,7 +20,11 @@ const getOffsetTopSnapshot = () =>
     : 0;
 
 export function useVisualViewportSize() {
-  const height = useSyncExternalStore(subscribe, getHeightSnapshot);
-  const offsetTop = useSyncExternalStore(subscribe, getOffsetTopSnapshot);
+  const height = useSyncExternalStore(subscribe, getHeightSnapshot, () => 0);
+  const offsetTop = useSyncExternalStore(
+    subscribe,
+    getOffsetTopSnapshot,
+    () => 0,
+  );
   return { height, offsetTop };
 }
